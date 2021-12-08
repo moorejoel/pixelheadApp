2d6fd26d0208ac3046d07cdeeae8950928d2e1a9
Carter Barnes <carter.barnes@cremadevelopment.com>
2021-12-07 21:29:48 -0800

    Create head and put it on the page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,20 +6,27 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { MatIconModule } from '@angular/material/icon';
 import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+
 import { OverlayListComponent } from './overlay-list/overlay-list.component';
+import { HeadComponent } from './head/head.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    OverlayListComponent
+    OverlayListComponent,
+    HeadComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     HttpClientModule,
+    MatIconModule,
     MatSliderModule,
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
